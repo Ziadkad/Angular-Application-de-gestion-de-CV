935c974bad7ffebb43526e931f3d18ed6e3d4c15
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-05-15 12:32:33 +0100

    navbarChanges

--- GestionCV/src/app/app-routing.module.ts
+++ GestionCV/src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SignupCandidatesComponent } from './components/signup-candidates/signup-candidates.component';
-
 import { SignupCompaniesComponent } from './components/signup-companies/signup-companies.component';
 import { SignupChooseComponent } from './components/signup-choose/signup-choose.component';
-import { HomeComponent } from './compoments/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { notAuthGuard } from './guards/not-auth.guard';
 import { roleGuard } from './guards/role.guard';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { OffersComponent } from './components/offers/offers.component';
 
 const routes: Routes = [
   { path:'', component: HomeComponent, canActivate:[authGuard]},
+  { path:'profile', component: ProfileComponent, canActivate:[authGuard]},
+  { path:'offers', component: OffersComponent},
   { path:'signin', component: SigninComponent,canActivate:[notAuthGuard]},
   { path:'signup', component: SignupComponent,canActivate:[notAuthGuard], children: [
     { path:'',component: SignupChooseComponent},
--- GestionCV/src/app/components/navbar/navbar.component.ts
+++ GestionCV/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+  public isAuthenticated: boolean = false;
+  public mobile: boolean = false;
+  public dropdown: boolean = false;
+  public infos:any;
+  public roles!:string;
+  ngDoCheck(){
+    this.infos=this.authService.userinfos;
+    this.roles=this.authService.roles;
+    this.isAuthenticated=this.authService.isAuthenticated;
+    if(!this.isAuthenticated){
+    this.dropdown=false;
+    this.mobile=false;
+  }
+  }
+  signOut() {
+    this.authService.logOut();
+    this.router.navigate([''])
+  }
+  mobileFunc(){
+    this.mobile = !this.mobile;
+  }
+  dropdownFunc(){
+    this.dropdown = !this.dropdown;
+  }
 }
